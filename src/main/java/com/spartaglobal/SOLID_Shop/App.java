da06a1ba0f0bc27a1d77799b3878a6890e78226e
Adrian Odbierzychleb <adrian.odbierzychleb@gmail.com>
2019-10-21 15:27:22 +0100

    initial commit

---
+++ src/main/java/com/spartaglobal/SOLID_Shop/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.SOLID_Shop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
