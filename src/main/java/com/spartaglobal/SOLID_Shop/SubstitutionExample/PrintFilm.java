c0fb047265ed5e71b77a48e382080dc1d442902b
Adrian Odbierzychleb <adrian.odbierzychleb@gmail.com>
2019-10-21 15:49:18 +0100

    Single responsibility example

---
+++ src/main/java/com/spartaglobal/SOLID_Shop/SubstitutionExample/PrintFilm.java
@@ -0,0 +1,9 @@
+package com.spartaglobal.SOLID_Shop.SubstitutionExample;
+
+public class PrintFilm extends Film {
+
+    public void printFilmName(){
+        System.out.println(getFilmName());
+    }
+
+}
